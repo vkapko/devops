4a31a684312a60dfc6833d03025949873835ea4d
Vitaliy Kapko <vkapko@gmail.com>
2026-02-07 14:03:25 -0700

    init commit

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "vkapko-test-bucket"  # Must be globally unique
+}
+
+# Create a VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name        = "main-vpc"
+    Environment = var.environment
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
